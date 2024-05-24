c5e4a863389679a557895d492af7eca297695d29
jayshri <nagargojejayshri020@gmail.com>
2024-05-24 12:20:26 +0530

    my first commit

--- Product/views/checkout.py
+++ Product/views/checkout.py
@@ -1,21 +1,24 @@
 from django.views import View
 from django.shortcuts import redirect
-from Product.model import register
 from Product.model.product import Product
 from Product.model.orders import Order
-from Product.templatetags import cart
+
 
 
 class CheckOut(View):
     def post(selfself, request):
         address = request.POST.get('address')
         phone = request.POST.get('phone')
-        register = request.session.get('register')
+        register = request.session.get('name')
         cart = request.session.get('cart')
         products = Product.get_product_by_id(list(cart.keys()))
-        print(address, phone, register, cart, products)
+        print(register, products)
         return redirect('cart')
 
-        for product in Product:
-            order = Order(register=register,product=product.id,price=product.price,quantity=cart.get())
+        for product in products:
+            order = Order(register=register,
+                          product=product.id,
+                          price=product.price,
+                          quantity=cart.get(str(product.id)))
+        request.session['cart']={}
         return redirect('cart')
